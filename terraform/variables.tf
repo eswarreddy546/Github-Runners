bbd2f66f537d3de1c2a8b8806dd47cd221fe403c
Asri <asri>
2026-03-13 10:37:42 +0530

    git hubrunners

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "eswar.xyz"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z00224221HVTOMR4RK7YI"
+  description = "description"
+}
